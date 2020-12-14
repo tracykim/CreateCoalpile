68797e298981197ecc836fd1305efb49ade6238c
tracykim <jinye.dw@foxmail.com>
2020-12-14 21:24:49 +0800

    2020_12_14 煤堆一侧靠墙

--- main.cpp
+++ main.cpp
@@ -10,53 +10,77 @@ using namespace std;
 
 int main()
 {
-    pcl::PointCloud<pcl::PointXYZ> cloud;
+    double r=50; // 圆圈起始半径
+    const double r_const = r;
+    const double boundary = 0.5 * r; // 在这之外的点剔除
+    cout<<"boundary: "<<boundary<<endl;
+    const double loop_diff =0.5; // 两个点之间相邻的度数
+    const double r_diff = 0.8; // 圆圈半径缩小长度
+
+    const double height = 25; // 煤堆高度
+    const int num_circle = 360 * 1/loop_diff; // 每一圈点的个数
+    const double h_diff = 0.5; // 每层煤堆之间的高度
+    const int num_layers = height/h_diff + 1; // 煤堆层数
+    const int num_coalpile = 3; // 煤堆个数
+    int num_points = 0; // 统计点云中的点数
 
     // Fill in the cloud data
-    int pile_size = 55; // z轴高度
-    cloud.width = 360;
-    cloud.height = pile_size*3;
+    pcl::PointCloud<pcl::PointXYZ> cloud;
+    cloud.width = num_circle*num_layers*num_coalpile;
+    cloud.height = 1;
     cloud.points.resize(cloud.width * cloud.height);
-
-    int point_num;
-    double r=40; // 半径
-    double r_const = r;
-    cout<<"cloud.height: "<<cloud.height<<endl;
+    cout<<"cloud points: "<<cloud.points.size()<<endl;
 
     //添加高斯噪声
     boost::mt19937 rng;
     rng.seed(static_cast<unsigned int>(time(0)));
-    boost::normal_distribution<> nd(0, 0.5);
-    boost::variate_generator<boost::mt19937&, boost::normal_distribution<> > var_nor(rng, nd);//产生随机/数
+    boost::normal_distribution<> nd(0, 0.1);
+    boost::variate_generator<boost::mt19937&, boost::normal_distribution<> > var_nor(rng, nd);//产生随机数
 
-    for(int z=0;z<pile_size;++z)
+    double z = 0;
+    while(z<height)
     {
         cout<<"z: "<<z<<",r: "<<r<<endl;
 
-        for(int loop=0;loop<360;++loop)
+        for(double loop=0;loop<360;loop=loop+loop_diff)
         {
-            point_num=z*360 + loop;
+            //point_num=z*360 + loop;
             float hudu = loop * PI / 180;
-            cloud.points[point_num].x = r*cos(hudu) + static_cast<float> (var_nor());
-            cloud.points[point_num].y = r*sin(hudu) + static_cast<float> (var_nor());
-            cloud.points[point_num].z = 0.5*(z+1.0f) + static_cast<float> (var_nor());
-            //或者为0.3*static_cast<float>(z);目地在于int转换为float
+//            if(hudu>2*PI/3 && hudu<4*PI/3)
+//                continue;
+            double x = r*cos(hudu) + static_cast<float> (var_nor());
+            if(x < -boundary)
+            {
+                //cout<<"x: "<<x<<endl;
+                continue;
+            }
+            cloud.points[num_points].x = x;
+            cloud.points[num_points].y = r*sin(hudu) + static_cast<float> (var_nor());
+            cloud.points[num_points].z = z + static_cast<float> (var_nor());
+            num_points++;
 
             // 第二个圆堆
-//            if(hudu>2*PI/3 && hudu<4*PI/3) //hudu<3/PI || hudu>5*PI/3
-//                continue;
-            cloud.points[cloud.width*pile_size + point_num].x = r*cos(hudu) + (r_const+15) + static_cast<float> (var_nor());
-            cloud.points[cloud.width*pile_size + point_num].y = r*sin(hudu) + 20 + static_cast<float> (var_nor());
-            cloud.points[cloud.width*pile_size + point_num].z = 0.45*(z+3.0f) + static_cast<float> (var_nor());
+            cloud.points[num_points].x = r*cos(hudu) + static_cast<float> (var_nor());
+            cloud.points[num_points].y = r*sin(hudu) + (1.6*r_const) + static_cast<float> (var_nor());
+            cloud.points[num_points].z = (z) + static_cast<float> (var_nor());
+            num_points++;
 
             // 第三个
-            cloud.points[cloud.width*pile_size*2 + point_num].x = r*cos(hudu) + (2*r_const+20); + static_cast<float> (var_nor());
-            cloud.points[cloud.width*pile_size*2 + point_num].y = r*sin(hudu) - 25 + static_cast<float> (var_nor());
-            cloud.points[cloud.width*pile_size*2 + point_num].z = 0.6*(z+2.0f) + static_cast<float> (var_nor());
+            cloud.points[num_points].x = r*cos(hudu)+ static_cast<float> (var_nor());
+            cloud.points[num_points].y = r*sin(hudu) - (1.6*r_const)  + static_cast<float> (var_nor());
+            cloud.points[num_points].z = (z) + static_cast<float> (var_nor());
+            num_points++;
         }
-        if (r > 0.6)
-            r=r-0.7;
+        if (r > r_diff)
+            r=r-r_diff;
+        z = z+h_diff;
     }
+    cout<<"num_points: "<<num_points<<endl;
+    cloud.width = num_points;
+    cloud.height = 1;
+    cloud.points.resize(cloud.width * cloud.height);
+    cout<<"cloud points: "<<cloud.points.size()<<endl;
+
     pcl::io::savePCDFile("cloud.pcd", cloud);
     return 0;
 }
