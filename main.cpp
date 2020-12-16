79561ecdec0ed737d4d8bcd37babb2fac1ce1d95
tracykim <jinye.dw@foxmail.com>
2020-12-16 10:45:03 +0800

    2020_12_16 把墙做成弧形

--- main.cpp
+++ main.cpp
@@ -7,27 +7,24 @@
 
 using namespace std;
 #define PI 3.1415926535
-const int num_coalpile = 3; // 煤堆个数
+const int num_coalpile = 2; // 煤堆个数
 
 int main()
 {
-    double R[num_coalpile]={50, 52, 48}; // 圆圈起始半径
+    double R[]={50, 50, 50}; // 圆圈起始半径
+    double HUDU[][2]={ {112, 248}, {65, 188} }; // 圆圈起始弧度
     double t_center[][2] = {
-        {0, 0},
-        //{0.034, 0.259},
-        //{0.134, 0.5},
-        //{0.293, 0.707},
-        //{0.5, 0.866},
-        {0, 1.2}, //{0.741, 0.966},
-        //{1, 1},
-        {0, 3.0}, //{1.5, 0.866},
+        {-1.5, 0},
+        {-0.75, 1.29}
+        //{0.75, 1.29}
     }; // 每个煤堆中心偏移的距离
     double r_const;
-    double boundary; // 在这之外的点剔除
     const double loop_diff =0.5; // 两个点之间相邻的度数
-    const double r_diff = 0.8; // 圆圈半径缩小长度
+    const double r_diff = 0.75; // 圆上下层的差
+    double init_hudu = 0.5; // 初始弧度数
+    const double delta_hudu = 0.06;
 
-    const double height[] = {25, 30, 20}; // 煤堆高度
+    const double height[] = {22, 22, 22}; // 煤堆高度
     const int num_circle = 360 * 1/loop_diff; // 每一圈点的个数
     const double h_diff = 0.5; // 每层煤堆之间的高度
     const int num_layers = height[0]/h_diff + 1; // 煤堆层数
@@ -35,7 +32,7 @@ int main()
 
     // Fill in the cloud data
     pcl::PointCloud<pcl::PointXYZ> cloud;
-    cloud.width = num_circle*num_layers*num_coalpile;
+    cloud.width = num_circle*num_layers*num_coalpile*3;
     cloud.height = 1;
     cloud.points.resize(cloud.width * cloud.height);
     cout<<"cloud points: "<<cloud.points.size()<<endl;
@@ -52,29 +49,63 @@ int main()
         cout<<"num_coalpile: "<<i<<endl;
         double r = R[i];
         r_const = R[i];
-        boundary = 0.5 * R[i];
-        for(double z=0; z<height[i]; z = z+h_diff)
+        double hudu1 = HUDU[i][0], hudu2 = HUDU[i][1];
+        init_hudu = 0.5;
+        for(double h=0; h<height[i]; h = h+h_diff)
         {
-            cout<<"z: "<<z<<",r: "<<r<<endl;
-            // 第一个煤堆
+            cout<<"h: "<<h<<",r: "<<r<<endl;
+
             for(double loop=0;loop<360;loop=loop+loop_diff)
             {
-                float hudu = loop * PI / 180;
-                double x = r*cos(hudu) + static_cast<float> (var_nor());
-
-                if(x < -boundary)
-                {
-                    //cout<<"x: "<<x<<endl;
+                if(loop > hudu1 && loop < hudu2 && h < 16)
                     continue;
-                }
-                cloud.points[num_points].x = x + r_const * t_center[i][0];
-                cloud.points[num_points].y = r*sin(hudu) + (r_const*t_center[i][1]) + static_cast<float> (var_nor());
-                cloud.points[num_points].z = z + static_cast<float> (var_nor());
+                float hudu = loop * PI / 180;
+                double x = r*cos(hudu) + (r_const * t_center[i][0]) + static_cast<float> (var_nor());
+                double y = r*sin(hudu) + (r_const * t_center[i][1]) + static_cast<float> (var_nor());
+                double z = h + static_cast<float> (var_nor());
+                cloud.points[num_points].x = x;
+                cloud.points[num_points].y = y;
+                cloud.points[num_points].z = z;
+                num_points++;
+
+                cloud.points[num_points].x = -x;
+                cloud.points[num_points].y = y;
+                cloud.points[num_points].z = z;
                 num_points++;
             }
 
+
             if (r > r_diff)
                 r=r-r_diff;
+            hudu1 += init_hudu; // 上层的圆圈画的大一些
+            hudu2 -= init_hudu;
+            init_hudu += delta_hudu;
+
+            // 把最上层的圆填满
+            if(h+h_diff >= height[i])
+            {
+                cout<<"到达最上层圆, r: "<<r<<endl;
+                for(double r_above = r; r_above>0;r_above=r_above-r_diff)
+                {
+                    for(double loop=0;loop<360;loop=loop+loop_diff)
+                    {
+                        float hudu = loop * PI / 180;
+                        double x = r_above*cos(hudu) + (r_const * t_center[i][0]) + static_cast<float> (var_nor());
+                        double y = r_above*sin(hudu) + (r_const * t_center[i][1]) + static_cast<float> (var_nor());
+                        double z = h + static_cast<float> (var_nor());
+                        cloud.points[num_points].x = x;
+                        cloud.points[num_points].y = y;
+                        cloud.points[num_points].z = z;
+                        num_points++;
+
+                        cloud.points[num_points].x = -x;
+                        cloud.points[num_points].y = y;
+                        cloud.points[num_points].z = z;
+                        num_points++;
+                    }
+                }
+            }
+
         }
     }
     cout<<"num_points: "<<num_points<<endl;
