95550c270975da40562b3ee2486bde7cee34e0f8
tracykim <jinye.dw@foxmail.com>
2020-12-16 16:51:55 +0800

    2020_12_16 非对称写法(有问题未修复)

--- main.cpp
+++ main.cpp
@@ -7,16 +7,17 @@
 
 using namespace std;
 #define PI 3.1415926535
-const int num_coalpile = 2; // 煤堆个数
+const int num_coalpile = 4; // 煤堆个数
 
 int main()
 {
-    double R[]={50, 50, 50}; // 圆圈起始半径
-    double HUDU[][2]={ {112, 248}, {65, 188} }; // 圆圈起始弧度
+    double R[]={50, 50, 50, 50}; // 圆圈起始半径
+    double HUDU[][2]={ {112, 248}, {65, 188}, {115, 352}, {68, 292} }; // 圆圈起始弧度
     double t_center[][2] = {
         {-1.5, 0},
-        {-0.75, 1.29}
-        //{0.75, 1.29}
+        {-0.75, 1.29},
+        {0.75, 1.29},
+        {1.5, 0}
     }; // 每个煤堆中心偏移的距离
     double r_const;
     const double loop_diff =0.5; // 两个点之间相邻的度数
@@ -24,7 +25,7 @@ int main()
     double init_hudu = 0.5; // 初始弧度数
     const double delta_hudu = 0.06;
 
-    const double height[] = {22, 22, 22}; // 煤堆高度
+    const double height[] = {22, 22, 22, 22}; // 煤堆高度
     const int num_circle = 360 * 1/loop_diff; // 每一圈点的个数
     const double h_diff = 0.5; // 每层煤堆之间的高度
     const int num_layers = height[0]/h_diff + 1; // 煤堆层数
@@ -50,15 +51,28 @@ int main()
         double r = R[i];
         r_const = R[i];
         double hudu1 = HUDU[i][0], hudu2 = HUDU[i][1];
-        init_hudu = 0.5;
+        init_hudu = 0.5;        
         for(double h=0; h<height[i]; h = h+h_diff)
         {
-            cout<<"h: "<<h<<",r: "<<r<<endl;
+            //cout<<"h: "<<h<<",r: "<<r<<endl;
 
             for(double loop=0;loop<360;loop=loop+loop_diff)
             {
-                if(loop > hudu1 && loop < hudu2 && h < 16)
-                    continue;
+                if(i==0 || i==1)
+                {
+                    //cout<<"i: "<<i<<endl;
+                    if(loop > hudu1 && loop < hudu2 && h < 16)
+                        continue;
+                }
+                else if(i==2 || i==3)
+                {
+                    //cout<<"i: "<<i<<endl;
+                    if((loop > 0 && loop < hudu1) || (loop > hudu2 && loop < 360))
+                    {
+                        cout<<"loop: "<<loop<<endl;
+                        continue;
+                    }
+                }
                 float hudu = loop * PI / 180;
                 double x = r*cos(hudu) + (r_const * t_center[i][0]) + static_cast<float> (var_nor());
                 double y = r*sin(hudu) + (r_const * t_center[i][1]) + static_cast<float> (var_nor());
@@ -68,17 +82,25 @@ int main()
                 cloud.points[num_points].z = z;
                 num_points++;
 
-                cloud.points[num_points].x = -x;
-                cloud.points[num_points].y = y;
-                cloud.points[num_points].z = z;
-                num_points++;
+//                cloud.points[num_points].x = -x;
+//                cloud.points[num_points].y = y;
+//                cloud.points[num_points].z = z;
+//                num_points++;
             }
 
 
             if (r > r_diff)
                 r=r-r_diff;
-            hudu1 += init_hudu; // 上层的圆圈画的大一些
-            hudu2 -= init_hudu;
+            if(i==0 || i==1)
+            {
+                hudu1 += init_hudu; // 上层的圆圈画的大一些
+                hudu2 -= init_hudu;
+            }
+            else if(i==2 || i==3)
+            {
+                hudu1 -= init_hudu; // 上层的圆圈画的大一些
+                hudu2 += init_hudu;
+            }
             init_hudu += delta_hudu;
 
             // 把最上层的圆填满
@@ -98,10 +120,10 @@ int main()
                         cloud.points[num_points].z = z;
                         num_points++;
 
-                        cloud.points[num_points].x = -x;
-                        cloud.points[num_points].y = y;
-                        cloud.points[num_points].z = z;
-                        num_points++;
+//                        cloud.points[num_points].x = -x;
+//                        cloud.points[num_points].y = y;
+//                        cloud.points[num_points].z = z;
+//                        num_points++;
                     }
                 }
             }
